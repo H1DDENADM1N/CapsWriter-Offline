dc981b8fd5951a3807f3ef35db696d9374a55d91
H1DDENADM1N <65646535+H1DDENADM1N@users.noreply.github.com>
2026-02-06 09:39:21 +0800

    Merge util\client\strip_punc.py from upstream/master

--- util/client/strip_punc.py
+++ util/client/strip_punc.py
@@ -2,4 +2,16 @@ from util.config import ClientConfig as Config
 
 
 def strip_punc(text: str) -> str:
-    return text.strip(Config.trash_punc)
+    """
+    消除末尾标点
+
+    Args:
+        text: 原始文本
+
+    Returns:
+        去除末尾标点后的文本
+    """
+    if not text:
+        return text
+    clean_text = text.rstrip(Config.trash_punc)
+    return clean_text if clean_text else text
